7b6fba27ab82396548fd717c957ad911feaa09de
Faizulmd13 <faizulmd1306@gmail.com>
2026-03-30 11:26:23 +0530

    import and wrap routerprovider with cartprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { CartProvider } from "./components/context/CartContext";
 import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>,
 );
