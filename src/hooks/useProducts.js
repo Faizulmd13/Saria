0532349d99611e33fe454b636f838b2b9b478f64
Faizulmd13 <faizulmd1306@gmail.com>
2026-03-30 11:21:40 +0530

    set up a hook to read prodcuts from api

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products") //'?limit=n' add to limit
+      .then((res) => {
+        if (!res.ok) throw new Error("Failed to fetch Products");
+        return res.json();
+      })
+      .then((data) => {
+        setProducts(data.products);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return { products, loading, error };
+};
+
+export default useProducts;
