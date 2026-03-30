d876b80558158a98c3bb78e59aa2327746451730
Faizulmd13 <faizulmd1306@gmail.com>
2026-03-30 11:58:55 +0530

    import components into cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,31 @@
+import { useCart } from "../context/CartContext";
+import CartItem from "../components/cart/CartItem";
+import Invoice from "../components/cart/Invoice";
+import { Link } from "react-router";
+
 const Cart = () => {
-  return <h1>Cart</h1>;
+  const { cart } = useCart();
+
+  if (cart.length === 0) {
+    return (
+      <div className="empty-cart">
+        <h2>"Your cart is empty"</h2>
+        <Link to="/shop">Buy something!</Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className="cart-page">
+      <h2>Your Shopping Cart</h2>
+      <div className="cart-items">
+        {cart.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+      <Invoice />
+    </div>
+  )
 };
 
 export default Cart;
